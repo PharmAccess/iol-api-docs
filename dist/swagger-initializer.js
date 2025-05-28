b95e3b08d1a8298176d4de9b1d9bc8cdad2c22e7
Chris Ihure <ihure.chris@gmail.com>
2025-05-28 12:42:37 +0000

    Update Swagger initializer to use new OpenAPI 3.0 specification for MNCH Program FHIR API

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,17 +3,12 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
-    dom_id: '#swagger-ui',
+    url: "mamatoto-api-spec.yaml",
+    dom_id: "#swagger-ui",
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
