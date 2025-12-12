270a9f4de81cdccb54fc124dda49d80725121525
Ihure <4818744+Ihure@users.noreply.github.com>
2025-12-12 10:05:24 +0000

    Update swagger-ui to v5.31.0

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,12 +3,17 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "mamatoto-api-spec.yaml",
-    dom_id: "#swagger-ui",
+    url: "swagger.yaml",
+    dom_id: '#swagger-ui',
     deepLinking: true,
-    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-    layout: "StandaloneLayout",
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
   });
 
   //</editor-fold>
